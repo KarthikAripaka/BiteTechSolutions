f808a77bb110ff364ae629ada8fc90b4eff4d0b0
Karthik1516devop <karthikaripaka482@gmail.com>
2026-01-26 15:29:18 +0530

    int

--- script.js
+++ script.js
@@ -32,6 +32,6 @@ const observer = new IntersectionObserver(entries => {
       });
     }
   });
-}, { threshold: 0.6 });
+}, { threshold: 0.3 });
 
 sections.forEach(section => observer.observe(section));
